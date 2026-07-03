c9d264c0d7b3ca19c9f6f200794fce6e5a164b6c
greshnobytela-dotcom <greshnobytela@gmail.com>
2026-07-04 00:44:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -25,8 +25,9 @@ case "${1:-}" in
   artifacts|forensics|bam|journal) run linux-artifacts.sh "${@:2}" ;;
   safemod|sessions|mc-sessions|safe) run safe-mod-detector.sh "${@:2}" ;;
   clean|cleaning|wipe)       run cleaning-detector.sh "${@:2}" ;;
+  ss|bypass|dualpc|faker|chameleon) run ss-bypass-detector.sh "${@:2}" ;;
   *)
-    echo "Использование: curl -fsSL URL/run.sh | bash -s -- {mods|doomsday|jni|inj|browser|downloads|artifacts|safemod|clean|sys|scan} [args]"
+    echo "Использование: curl -fsSL URL/run.sh | bash -s -- {mods|doomsday|jni|inj|browser|downloads|artifacts|safemod|clean|ss|sys|scan} [args]"
     exit 1
     ;;
 esac
